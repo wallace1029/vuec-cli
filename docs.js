a78cd0a5221690d52f6c8b54bfc9143d95d21a2b
王由之 <youzhi.wang@geely.com>
2019-03-22 09:34:40 +0800

    feat: create base cli code

---
+++ docs.js
@@ -0,0 +1,9 @@
+module.exports = {
+  explain() {
+    console.log('说明：')
+    console.log(`使用vuec create -t <template>快速创建一个模板`)
+    console.log(`template将会支持vue、element、sweet，目前默认并且只支持vue。`)
+    console.log('例如：')
+    console.log(`vuec create -t vue`)
+  }
+}
--- vuec.js
+++ vuec.js
@@ -6,27 +6,30 @@ const shell = require('shelljs')
 const path = require('path')
 const fs = require('fs')
 
+const docs = require('./docs')
+
 program
   .version('1.0.0')
   .description('Easy to build vue component project.')
 
 program
-  // 命名命令，必选参数componentName,传入action函数第一个参数
+// 命名命令，必选参数componentName,传入action函数第一个参数
   .command('create <componentName>')
   // 可选参数，确定使用的ui库，只能传入指定值，默认'vue'
-  .option('-U, --ui [ui]', '选择ui框架', /^(vue|element|sweet)$/i, 'vue')
+  .option('-t, --template <template>', '选择ui框架', /^(vue|element|sweet)$/i, 'vue')
   .action(function (componentName, options) { // 执行命令的的函数
-    // 从optiosn中获取ui
-    const {ui} = options
+    // 从optiosn中获取template
+    const {template} = options
     // 获取当前命令的路径
     const pwd = shell.pwd()
     // 获取项目的最终存放路径
     const targetPath = path.join(pwd.toString(), componentName)
-    const repository = 'https://github.com/youzhiwang/sweet-banner'
+    const repository = `https://github.com/youzhiwang/${template}-component`
     console.log('正在获取组件模板，请耐心等待...')
     clone(repository, targetPath).then(res => {
       // 删除.git文件
       shell.rm('-rf', path.join(targetPath, '.git'))
+      // 改写package.json中的部分配置
       fs.readFile(path.join(targetPath, './package.json'), 'utf8', (err, data) => {
         if (err) throw err
         let list = JSON.parse(data)
@@ -35,6 +38,7 @@ program
         fs.writeFile(path.join(targetPath, './package.json'), newContent, 'utf8', err => {
           if (err) throw err
           console.log('模板下载完成！')
+          docs.explain()
         })
       })
     })
@@ -42,11 +46,7 @@ program
 
 program
   .on('--help', () => {
-    console.log('Example:')
-    console.log('  创建一个默认的项目')
-    console.log('     $ efox create  或者 $ efox c')
-    console.log('  创建指定平台(web/mobile/ie8)、指定开发框架(vue/react/jQuery/angular)、指定项目名的项目')
-    console.log('     $ efox create testname -p web -f react \n')
+    docs.explain()
   })
 
 program.parse(process.argv)
