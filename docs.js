9176ff8f14d7b81887f5b8f5b83800da8d638003
王由之 <youzhi.wang@geely.com>
2019-04-08 09:35:48 +0800

    fix: Fix doc

--- docs.js
+++ docs.js
@@ -2,10 +2,10 @@ module.exports = {
   explain() {
     console.log(``)
     console.log(`说明：`)
-    console.log(` 使用vuec create -t <template>快速创建一个模板`)
+    console.log(` 使用vuec create <component-name> -t <template>快速创建一个模板`)
     console.log(` template将会支持vue、element、sweet，目前默认并且只支持vue。`)
     console.log(``)
     console.log(`例如：`)
-    console.log(` vuec create -t vue`)
+    console.log(` vuec create my-component -t vue`)
   }
 }
